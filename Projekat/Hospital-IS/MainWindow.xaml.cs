f370f849e8d7c45c9ad004453694a47e9d59a5e0
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2021-03-29 19:18:22 +0200

    Izmena kogin prozora

--- Projekat/Hospital-IS/MainWindow.xaml.cs
+++ Projekat/Hospital-IS/MainWindow.xaml.cs
@@ -21,6 +21,20 @@ namespace Hospital_IS
 
         private void Login(object sender, RoutedEventArgs e)
         {
+            if (email.Text == "upravnik@gmail.com" && password.Password.ToString() == "upravnik")
+            {
+            }
+            else if (email.Text == "doktor@gmail.com" && password.Password.ToString() == "doktor")
+            {
+            }
+            else if (email.Text == "sekretar@gmail.com" && password.Password.ToString() == "sekretar")
+            {
+                SecretaryMainWindow smw = new SecretaryMainWindow();
+                smw.Show();
+            }
+            else if (email.Text == "pacijent@gmail.com" && password.Password.ToString() == "pacijent")
+            {
+            }
         }
     }
 }
