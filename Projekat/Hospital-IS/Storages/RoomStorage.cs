18e91f76d86912592cd33f609e02a87de800f0e4
VladimirJ123 <jovanovic.ra128.2018@uns.ac.rs>
2021-03-30 01:30:32 +0200

    serijalizacija po listama

--- Projekat/Hospital-IS/Storages/RoomStorage.cs
+++ Projekat/Hospital-IS/Storages/RoomStorage.cs
@@ -1,28 +1,47 @@
 using Model;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace Storages
 {
     public class RoomStorage
     {
-        private String fileLocation;
+        private String fileLocation ;
 
-        public List<Room> GetAll()
+
+        public RoomStorage()
         {
-            Room r1 = new Room(RoomType.ConsultingRoom, false, true, 2, 25);
+            this.fileLocation = "../../../FileStorage/rooms.json";
+        }
+
+        public ObservableCollection<Room>  GetAll()
+        {
+            /*Room r1 = new Room(RoomType.ConsultingRoom, false, true, 2, 25);
             Room r2 = new Room(RoomType.ConsultingRoom, false, true, 1, 11);
 
             List<Room> all = new List<Room>();
             all.Add(r2);
             all.Add(r1);
-            return all;
+            return all;*/
+
+            String text = File.ReadAllText(this.fileLocation);
+            ObservableCollection<Room>  allRooms  = JsonConvert.DeserializeObject<ObservableCollection<Room>>(text);
+            return allRooms;
+
         }
 
-        public void SaveRoom(Model.Room newRoom)
+        public void SaveRooms(ObservableCollection<Room> allRooms)
         {
-            throw new NotImplementedException();
+            
+
+            var file = JsonConvert.SerializeObject(allRooms, Formatting.Indented);
+            using (StreamWriter writer = new StreamWriter(this.fileLocation))
+            {
+                writer.Write(file);
+            }
         }
 
     }
