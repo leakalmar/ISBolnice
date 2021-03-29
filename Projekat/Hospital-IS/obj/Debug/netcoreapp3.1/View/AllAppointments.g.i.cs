9d1447f274899a1b65714ae506f2dcef47c3a332
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2021-03-29 20:02:12 +0200

    Merge

--- Projekat/Hospital-IS/obj/Debug/netcoreapp3.1/View/AllAppointments.g.i.cs
+++ Projekat/Hospital-IS/obj/Debug/netcoreapp3.1/View/AllAppointments.g.i.cs
@@ -95,13 +95,13 @@ namespace Hospital_IS.View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Hospital-IS;component/view/allappointments.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Hospital-IS;V1.0.0.0;component/view/allappointments.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\AllAppointments.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace Hospital_IS.View {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
