6b2fb1f742876216354ccf57d93e24f2184d6c29
Lea Kalmar <73386599+leakalmar@users.noreply.github.com>
2021-03-30 16:00:00 +0200

    Male izmene

--- Projekat/Hospital-IS/MainWindow.xaml.cs
+++ Projekat/Hospital-IS/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ namespace Hospital_IS
             InitializeComponent();
         }
 
+
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
