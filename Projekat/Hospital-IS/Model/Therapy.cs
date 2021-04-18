c7097a0249e6feece5e4103d398442905e0df520
Nenad Becanovic <nenadbecanovic99@gmail.com>
2021-04-18 20:42:15 +0200

    Dodata satnica uzimanja leka

--- Projekat/Hospital-IS/Model/Therapy.cs
+++ Projekat/Hospital-IS/Model/Therapy.cs
@@ -11,6 +11,7 @@ namespace Model
         public int TimesADay { get; set; }
         public DateTime TherapyStart { get; set; }
         public DateTime TherapyEnd { get; set; }
+        public int FirstUsageTime { get; set; }
 
         public Therapy(Medicine medicine, int quantity, int timesADay, DateTime start, DateTime end)
         {
