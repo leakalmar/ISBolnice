bba5707b7e7954d7fb58c0cf26ee2b972de45645
VladimirJ123 <jovanovic.ra128.2018@uns.ac.rs>
2021-04-28 11:41:14 +0200

    dodati servisi za korisnike i zaposlene

---
+++ Projekat/Hospital-IS/Service/AdministrationEmployeeService.cs
@@ -0,0 +1,31 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class AdministrationEmployeeService
+    {
+
+        public List<Employee> AllEmployees { get; set; }
+
+        private static AdministrationEmployeeService instance = null;
+        public static AdministrationEmployeeService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AdministrationEmployeeService();
+                }
+                return instance;
+            }
+        }
+
+        private AdministrationEmployeeService()
+        {
+            
+        }
+    }
+}
---
+++ Projekat/Hospital-IS/Service/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class UserService
+    {
+        public List<User> AllUsers { get; set; }
+
+        private static UserService instance = null;
+        public static UserService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserService();
+                }
+                return instance;
+            }
+        }
+
+        private UserService()
+        {
+
+        }
+    }
+}
