cd057142a153320fe2652e433616cdea23780e3c
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2021-05-30 04:35:42 +0200

    Implementacija menubar-a

--- Projekat/Hospital-IS/MainWindow.xaml.cs
+++ Projekat/Hospital-IS/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Hospital_IS.Controllers;
 using Hospital_IS.DoctorView;
 using Hospital_IS.DTOs;
 using Hospital_IS.ManagerView1;
+using Hospital_IS.SecretaryView;
 using Hospital_IS.Storages;
 using Hospital_IS.View;
 using Model;
