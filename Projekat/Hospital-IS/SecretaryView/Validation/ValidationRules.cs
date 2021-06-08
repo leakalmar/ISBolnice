01068a0c111cd726d10b5c175457d65aee49b5ca
ABjelobaba <bjelobaba.ra136.2018@uns.ac.rs>
2021-06-08 07:55:05 +0200

    Validacija notifikacija

---
+++ Projekat/Hospital-IS/SecretaryView/Validation/ValidationRules.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Hospital_IS.SecretaryView.Validation
+{
+    class IsEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var s = value as string;
+            if (string.IsNullOrEmpty(s))
+            {
+                return new ValidationResult(false, "Polje je obavezno.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+
+    class IsFirstLetterInUppercaseValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var s = value as string;
+            if (!s[0].ToString().Equals(s[0].ToString().ToUpper()))
+            {
+                return new ValidationResult(false, "Prvo slovo mora biti veliko.");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
