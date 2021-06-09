eb1153e1f43b8ed64750652e45d5242a22054877
Nenad Becanovic <nenadbecanovic99@gmail.com>
2021-06-10 00:04:16 +0200

    Implementiran Interface Segregation Principle kao User interfejs

--- Projekat/Hospital-IS/Model/Employee.cs
+++ Projekat/Hospital-IS/Model/Employee.cs
@@ -1,15 +1,36 @@
+using Enums;
+using Hospital_IS.Model.Interface;
 using System;
 using System.Collections.Generic;
 
 namespace Model
 {
-    public class Employee : User
+    public class Employee : IEmployeeUser
     {
+        public EducationCategory Education { get; set; }
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public String Phone { get; set; }
+        public String Email { get; set; }
+        public String Gender { get; set; }
+        public String Relationship { get; set; }
+        public String Password { get; set; }
+        public String Address { get; set; }
         public Double Salary { get; set; }
         public DateTime EmploymentDate { get; set; }
+        public List<WorkDay> WorkDay { get; set;}
 
-        public Employee(int id, string name, string surname, DateTime birthDate, string address, string email, string password, double salary, DateTime employmentDate, List<WorkDay> workDays) : base(id, name, surname, birthDate, address, email, password)
+        public Employee(int id, string name, string surname, DateTime birthDate, string address, string email, string password, double salary, DateTime employmentDate, List<WorkDay> workDays)
         {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.BirthDate = birthDate;
+            this.Address = address;
+            this.Email = email;
+            this.Password = password;
             this.Salary = salary;
             this.EmploymentDate = employmentDate;
             this.WorkDay = workDays;
@@ -17,11 +38,6 @@ namespace Model
 
         public Employee()
         {
-        }
-
-        public List<WorkDay> WorkDay
-        {
-            get; set;
         }
 
         public void AddWorkDay(WorkDay newWorkDay)
@@ -48,6 +64,5 @@ namespace Model
             if (WorkDay != null)
                 WorkDay.Clear();
         }
-
     }
 }
