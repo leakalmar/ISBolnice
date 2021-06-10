0eeec0035205fc2f851eb68018f619d2fbfb8257
Nenad Becanovic <nenadbecanovic99@gmail.com>
2021-06-10 12:04:08 +0200

    Izbacene metode iz Employee model klase

--- Projekat/Hospital-IS/Model/Employee.cs
+++ Projekat/Hospital-IS/Model/Employee.cs
@@ -40,30 +40,5 @@ namespace Model
         public Employee()
         {
         }
-
-        public void AddWorkDay(WorkDay newWorkDay)
-        {
-            if (newWorkDay == null)
-                return;
-            if (this.WorkDay == null)
-                this.WorkDay = new List<WorkDay>();
-            if (!this.WorkDay.Contains(newWorkDay))
-                this.WorkDay.Add(newWorkDay);
-        }
-
-        public void RemoveWorkDay(WorkDay oldWorkDay)
-        {
-            if (oldWorkDay == null)
-                return;
-            if (this.WorkDay != null)
-                if (this.WorkDay.Contains(oldWorkDay))
-                    this.WorkDay.Remove(oldWorkDay);
-        }
-
-        public void RemoveAllWorkDay()
-        {
-            if (WorkDay != null)
-                WorkDay.Clear();
-        }
     }
 }
